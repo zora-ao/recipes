1b8b74ebefa2a7a0347b4e65822c666ebd8eebbe
Carl Jasper Ramos <carljasperramos97@gmail.com>
2025-08-23 02:04:36 +0800

    i'll sleep

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { createRoutesFromElements, Route, RouterProvider, createBrowserRouter } from "react-router-dom"
+import Home from "./pages/Home"
+import Recipe from "./pages/Recipe"
+import Favorites from "./pages/Favorites"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route index element={<Home />} /> 
+      <Route path="/recipe/:id" element={< Recipe/>} />
+      <Route path="/favorites" element={<Favorites />} />
+    </>
+  )
+)
+
+const App = () => {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
